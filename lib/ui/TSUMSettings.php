35624d87fff0ad387a9ef0318b029247a62b9221
marconagel <marconagel@localhost>
2024-04-17 21:02:50 +0200

    added database connection for pc retrieval

--- lib/ui/TSUMSettings.php
+++ lib/ui/TSUMSettings.php
@@ -14,11 +14,18 @@ defined( 'ABSPATH' ) or die( 'Direct access not allowed!' );
 
 class TSUMSettings {
     
-    public function __construct() {      
+    private $db_pfx = '';
+    
+    public function __construct() {     
+        //get db prefix
+        global $wpdb;
+        $this->db_pfx = $wpdb->prefix;
         //add menu for setting page
         add_action( 'admin_menu', array( $this, 'tsumSettingsPage' ) ); 
         //register settings
         add_action( 'admin_init', array( $this, 'tsumRegisterSettings' ) );
+        //load data handler
+        require_once TSU_MC_PLUGIN_PATH . '/lib/util/TSUMDataHandler.php';
     }
     
     public function tsumSettingsPage() {
@@ -32,6 +39,11 @@ class TSUMSettings {
     }
     
     public function tsumRenderSettingsPage() {
+        
+        //load options
+        $options = get_option( 'tsumMCOptions' );
+        $tsumDataHandler = new \lib\util\TSUMDataHandler( isset( $options['tsum_general_setting_db_table'] ) ? $options['tsum_general_setting_db_table'] : '' ); 
+        
         ?>
         <div class="wrap">
             <h1><?php echo esc_html__( 'MapConnect Settings', 'tsu-mapconnect' ); ?></h1>
@@ -41,26 +53,93 @@ class TSUMSettings {
                 do_settings_sections( 'tsu-mapconnect' ); ?>
                 <input name="submit" class="button button-primary" type="submit" value="<?php esc_attr_e( 'Save' ); ?>" />
             </form>
+            <p></p>
+            <hr>
+            <h2><?php echo esc_html__( 'Database connection overview', 'tsu-mapconnect' ); ?></h2>
+            <?php 
+                $tsumDataHandler->tsumPrintConnectionDataTable();
+            ?>
         </div>
         <?php
     }  
     public function tsumRegisterSettings() {
-        register_setting( 'tsumMCOptions', 'tsumMCOptions', 'tsumMCOptionsValidate' );
-        add_settings_section( 'tsum_general_settings', esc_html__( 'General Settings', 'tsu-mapconnect' ), array( $this, 'tsumGenSettingsSectionText' ), 'tsu-mapconnect' );
+        
+        $args = array( 'sanitize_callback' => array( $this, 'tsumMCOptionsValidate' ) );
+        
+        register_setting( 'tsumMCOptions', 'tsumMCOptions', $args );
+        
+        add_settings_section( 
+                'tsum_general_settings', 
+                esc_html__( 'General Settings', 'tsu-mapconnect' ), 
+                array( $this, 'tsumGenSettingsSectionText' ), 
+                'tsu-mapconnect' );
 
-        add_settings_field( 'tsum_general_setting_pc_output', esc_html__( 'enable postcode field in Metabox', 'tsu-mapconnect' ), array( $this, 'tsumGenSettingsPCOutput' ), 'tsu-mapconnect', 'tsum_general_settings' );
+        add_settings_field( 
+                'tsum_general_setting_pc_output', 
+                esc_html__( 'enable postcode field in Metabox', 'tsu-mapconnect' ), 
+                array( $this, 'tsumGenSettingsPCOutput' ), 
+                'tsu-mapconnect', 
+                'tsum_general_settings' );
+        
+        add_settings_field( 
+                'tsum_general_setting_db_table', 
+                esc_html__( 'database table holding parameters', 'tsu-mapconnect' ), 
+                array( $this, 'tsumGenSettingsDBTable' ), 
+                'tsu-mapconnect', 
+                'tsum_general_settings' );   
+            
     } 
     
+    //TODO: add tsum_general_setting_db_table to validation
     public function tsumMCOptionsValidate( $input ) {
         
         $newinput = [];
+        $sql_table_pattern = "/^[a-zA-Z_][a-zA-Z0-9_]*$/";
+        $success_msg = esc_html__( 'Settings successfully updated!', 'tsu-mapconnect' );
+        $errors = false;
+        $msg = [];
+                
+        $validate = [];
+        $validate['pc'] = $input['tsum_general_setting_pc_output'] ?? null;
+        $validate['tablename'] = $input['tsum_general_setting_db_table'] ?? null;
         
-        if ( $input[ 'tsum_general_setting_pc_output' ] === '0' || $input[ 'tsum_general_setting_pc_output' ] === '1' ) {
-            $newinput['tsum_general_setting_pc_output'] = $input[ 'tsum_general_setting_pc_output' ];
-        } else {
-            $newinput['tsum_general_setting_pc_output'] = '1';
+        //validate boolean
+        if ($validate['pc'] != null) {
+            if ( $input['tsum_general_setting_pc_output'] === '0' || $input['tsum_general_setting_pc_output'] === '1' ) {
+                $newinput['tsum_general_setting_pc_output'] = $input[ 'tsum_general_setting_pc_output' ];       
+            } else {
+                $newinput['tsum_general_setting_pc_output'] = '1';
+                $errors = true;
+                $msg[0] = esc_html__( 'Setting for postcode output could not be validated. Resetted field to default value.', 'tsu-mapconnect' );
+            }
         }
+        if ($validate['tablename'] != null) {
+            //validate sql table name
+            if ( preg_match($sql_table_pattern, $input['tsum_general_setting_db_table']) ) {
+                $newinput['tsum_general_setting_db_table'] = $input[ 'tsum_general_setting_db_table' ];          
+            } else {
+                $newinput['tsum_general_setting_db_table'] = "";
+                $errors = true;
+                $msg[1] = esc_html__( 'Inputted name of database table is not allowed.', 'tsu-mapconnect' );
+            }
+        }
+        
+        if ($errors) {
+            $error_code = isset($msg[1]) ? 1 : 0;
+            $error_desc = '';
+            
+            if (count($msg) > 0) {
+                $error_desc .= '<ul>';
+                $error_desc .= '<li>' . implode('</li><li>', $msg) . '</li>';
+                $error_desc .= '</ul>';
+            } else {
+                $error_desc .= esc_html__('Undefined error(s) - contact some higher instance.', 'tsu-mapconnect');
+            }
 
+            $message = esc_html__( 'The following error(s) occured:', 'tsu-mapconnect' ) . $error_desc;            
+            add_settings_error( 'tsumMCOptions', $error_code, $message );  
+            
+        }
         return $newinput;
     }
     
@@ -79,6 +158,22 @@ class TSUMSettings {
                 <?php checked( '1', $options['tsum_general_setting_pc_output'] ?? 0 ); ?>
             /> 
         <?php        
+    }
+    
+    public function tsumGenSettingsDBTable() {
+        
+        $options = get_option( 'tsumMCOptions' );
+        ?>  
+            <code class="wp-prefix"><?php echo $this->db_pfx; ?></code>
+            <input 
+                id='tsum_general_setting_db_table' 
+                name='tsumMCOptions[tsum_general_setting_db_table]' 
+                type='text' 
+                value='<?php echo isset( $options['tsum_general_setting_db_table'] ) ? $options['tsum_general_setting_db_table'] : '' ?>'
+                placeholder='<?php echo esc_html__( 'enter db table name', 'tsu-mapconnect' ); ?>'
+            /> 
+            <p class="description"><?php echo esc_html__( 'database table holding parameters for external postcode database access. Prefix as shown before input field will be added automatically.', 'tsu-mapconnect' ); ?></p>
+        <?php         
     }
     
 }
--- lib/util/TSUMDataHandler.php
+++ lib/util/TSUMDataHandler.php
@@ -4,30 +4,93 @@ namespace lib\util;
 /**
  * Description of TSUMDataHandler
  * 
- * Class for handling external data stuff, like exporting geojson from external api
- * non-static, must be normally invoked. Needs an URI to fetch from to be passed on construction.
+ * Class for handling external data stuff
  *
  * @author marconagel
  */
 class TSUMDataHandler {
     
-    private $tsumFetchURL;
-    private $tsumAppend;            
-    private $tsumVarArr; 
+    private $tsumParamTable;
+    private $tsumParams;
+    private $tsumPFX;
+    private $tsumWPDB;
     
    /**
     * 
-    * @param string $url - URL needs to be passed on construct. This is NOT being escaped or sanitized.
-    *                      URL should lead to place where stuff could be taken.
-    * @param boolean $append - true if parameter should be appended to url. if set to false, you can define an
-    *                          array of variables attached to query string.
+    * @param string $table - Database table in WordPress db holding params to connect to external table
     */
-    public function __construct ( $url, $append = true, $vars = [] ) {       
-        $this->tsumFetchURL = $url;
-        $this->tsumAppend = $append;
-        !empty($vars) ? $this->tsumVarArr = $vars : null;
-    }
-    public function tsumFetchJSONAreaData ( $area ) {
+    public function __construct ( $table, $params = [ 
+                                                        'host' => 'ig_event_db_host', 
+                                                        'db' => 'ig_event_db_name',
+                                                        'user' => 'ig_event_db_user',
+                                                        'password' => 'ig_event_db_password',        
+                                                    ] ) {       
+        global $wpdb;
         
+        $this->tsumWPDB = $wpdb;
+        $this->tsumParamTable = $table;
+        $this->tsumParams = $params;
+        $this->tsumPFX = $wpdb->prefix;
+    }
+    public function tsumPrintConnectionDataTable() {
+        //query database
+        $prefixedTable = $this->tsumPFX . $this->tsumParamTable;
+        $paramData = $this->tsumWPDB->get_results( "SELECT parameter, value FROM $prefixedTable" );   
+        
+        if ( $paramData != null ): ?> 
+            <table class="widefat striped">
+                <tbody>
+                    <tr>
+                        <td><?php echo esc_html__( 'Parameters Table', 'tsu-mapconnect' );  ?></td>
+                        <td><?php echo $prefixedTable; ?></td>
+                    </tr>
+                    <?php
+                        $output = '<tr><td>' . esc_html__( 'Table found but no parameters set.', 'tsu-mapconnect' ) . '</td></tr>';
+                        $paramsSet = false;
+                        
+                        foreach($paramData as $param) {
+                            
+                            $paramName = '';
+                            $paramVal = '';
+                            
+                            if( $this->tsumParams['host'] == $param->parameter ) {
+                                $paramName = esc_html__( 'Database Host', 'tsu-mapconnect' );
+                                $paramVal = $param->value;
+                            } else if( $this->tsumParams['db'] == $param->parameter ) {
+                                $paramName = esc_html__( 'Database Name', 'tsu-mapconnect' );
+                                $paramVal = $param->value;
+                            } else if( $this->tsumParams['user'] == $param->parameter ) {
+                                $paramName = esc_html__( 'Database User', 'tsu-mapconnect' );
+                                $paramVal = $param->value;
+                            }  else if( $this->tsumParams['password'] == $param->parameter ) {
+                                $paramName = esc_html__( 'Database Pass', 'tsu-mapconnect' );
+                                $paramVal = '********';
+                            }
+                            if ( $paramName != '' && $paramVal != '' ) {
+                                if ( !$paramsSet ) { 
+                                    $output = '';
+                                    $paramsSet = true;
+                                }
+                                
+                                $html = '<tr>';
+                                $html .= '<td>' . $paramName . '</td>';
+                                $html .= '<td>' . $paramVal . '</td>';
+                                $html .= '</tr>';
+                                $output .= $html;
+                            }   
+                        }
+                        echo $output;
+                    ?>                    
+                </tbody>
+            </table>
+            </p>
+        <?php else: ?>
+            <p>
+                <?php 
+                    echo '<b>' . ( isset( $this->tsumParamTable ) && $this->tsumParamTable != '' ? $this->tsumPFX . $this->tsumParamTable : '---' )  . '</b>: ' . 
+                            esc_html__( 'Table not found or defined. No connection established.', 'tsu-mapconnect' ); 
+                ?>
+            </p>
+        <?php endif;
     }
 }
